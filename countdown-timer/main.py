6b1351104d8fcee4b457294a0c44e86b6e2844c1
Behdad Tabrizi <b.tabrizi@deroso.de>
2026-03-05 21:44:53 +0100

    update countdown timer

--- countdown-timer/main.py
+++ countdown-timer/main.py
@@ -1,1 +1,12 @@
 # create countdown timer
+
+import time
+
+seconds = int(input("Enter the number of seconds for the countdown: "))
+
+while seconds > 0:
+    print(f"seconds remaining: {seconds} ", end="\r")  # overwrite the same line
+    time.sleep(1)
+    seconds -= 1
+
+print("Time's up!")
